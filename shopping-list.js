a0f18667bc63da00138056088c0fbbd6d8de167d
bryanmardono <bryanmardono1@gmail.com>
2024-01-09 16:28:21 +0800

    Created a simple shopping list through DOM manipulation

--- shopping-list.js
+++ shopping-list.js
@@ -1,5 +1,24 @@
 const list = document.querySelector('.list')
-
-const div = document.querySelector('#container')
-
+const input = document.querySelector('#item')
 const btn = document.querySelector('#btn')
+
+function addToList() {
+    const item = input.value;
+    input.value = '';
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listText.textContent = item;
+    listItem.appendChild(listText);
+    listBtn.textContent = 'Delete';
+    listItem.appendChild(listBtn);
+    list.appendChild(listItem);
+
+    listBtn.addEventListener('click', () => {
+        list.removeChild(listItem);
+    });
+}
+
+btn.addEventListener('click', addToList);
